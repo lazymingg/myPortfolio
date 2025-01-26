d3e65e4bf7e3aae44c6bc5bbb52ceac6a6ac98f3
lazymingg <nvminh232@clc.fitus.edu.vn>
2025-01-26 12:24:52 +0700

    seperate the file

---
+++ infomation_animation/js/welcomePopup.js
@@ -0,0 +1,84 @@
+function showPopup() {
+    let popup = document.getElementById("popup");
+    document.getElementById("overlay").style.display = "flex";
+    popup.style.animation = "move 1s forwards";
+  }
+  
+  function hidePopup() {
+    let popup = document.getElementById("popup");
+    document.getElementById("overlay").style.display = "none";
+    popup.style.animation = "none";
+  }
+  
+  function setupTypewriter(t) {
+    let HTML = t.innerHTML;
+    t.innerHTML = "";
+  
+    let cursorPosition = 0,
+      tag = "",
+      writingTag = false,
+      tagOpen = false,
+      typeSpeed = 10,
+      tempTypeSpeed = 0;
+  
+    let type = function () {
+      if (writingTag === true) {
+        tag += HTML[cursorPosition];
+      }
+  
+      if (HTML[cursorPosition] === "<") {
+        tempTypeSpeed = 0;
+        if (tagOpen) {
+          tagOpen = false;
+          writingTag = true;
+        } else {
+          tag = "";
+          tagOpen = true;
+          writingTag = true;
+          tag += HTML[cursorPosition];
+        }
+      }
+  
+      if (!writingTag && tagOpen) {
+        tag.innerHTML += HTML[cursorPosition];
+      }
+  
+      if (!writingTag && !tagOpen) {
+        if (HTML[cursorPosition] === " ") {
+          tempTypeSpeed = 0;
+        } else {
+          tempTypeSpeed = Math.random() * typeSpeed + 10;
+        }
+        t.innerHTML += HTML[cursorPosition];
+      }
+  
+      if (writingTag === true && HTML[cursorPosition] === ">") {
+        tempTypeSpeed = Math.random() * typeSpeed + 10;
+        writingTag = false;
+        if (tagOpen) {
+          let newSpan = document.createElement("span");
+          t.appendChild(newSpan);
+          newSpan.innerHTML = tag;
+          tag = newSpan.firstChild;
+        }
+      }
+  
+      cursorPosition += 1;
+      if (cursorPosition < HTML.length - 1) {
+        setTimeout(type, tempTypeSpeed);
+      }
+    };
+  
+    return {
+      type: type,
+    };
+  }
+  
+  let typer = document.getElementById("typewriter");
+  
+  let typewriter = setupTypewriter(typer);
+  
+  typewriter.type();
+  // showPopup();
+  hidePopup();
+  
