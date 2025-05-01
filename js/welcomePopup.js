d44071c2ca8ade790f1bf128361c7c42afed5ad8
lazymingg <nvminh232@clc.fitus.edu.vn>
2025-05-01 13:46:35 +0700

    adding left margin for the social bar

--- js/welcomePopup.js
+++ js/welcomePopup.js
@@ -90,4 +90,13 @@ function showPopup() {
   showPopup();
   // hidePopup();
 
+function hambergerMenu() {
+    let menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+
   
